e6f3035c5ca67cb4cd73a3bc763f18d4e1b0b34e
Wayner Barrios <waybarrios@gmail.com>
2022-02-18 12:33:07 -0500

    adding class attention

--- waynet.py
+++ waynet.py
@@ -46,6 +46,73 @@ class Mlp(nn.Module):
         x = self.drop(x)
         return x
 
+
+class Class_Attention(nn.Module):
+    # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+    # with slight modifications to do CA 
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0.):
+        super().__init__()
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.k = nn.Linear(dim, dim, bias=qkv_bias)
+        self.v = nn.Linear(dim, dim, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+    
+    def forward(self, x ):
+        
+        B, N, C = x.shape
+        q = self.q(x[:,0]).unsqueeze(1).reshape(B, 1, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+        k = self.k(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        q = q * self.scale
+        v = self.v(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        attn = (q @ k.transpose(-2, -1)) 
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x_cls = (attn @ v).transpose(1, 2).reshape(B, 1, C)
+        x_cls = self.proj(x_cls)
+        x_cls = self.proj_drop(x_cls)
+        
+        return x_cls  
+
+class LayerScale_Block_CA(nn.Module):
+    # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
+    # with slight modifications to add CA and LayerScale
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, Attention_block = Class_Attention,
+                 Mlp_block=Mlp,init_values=1e-4):
+        super().__init__()
+        self.norm1 = norm_layer(dim)
+        self.attn = Attention_block(
+            dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = Mlp_block(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.gamma_1 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
+        self.gamma_2 = nn.Parameter(init_values * torch.ones((dim)),requires_grad=True)
+
+    
+    def forward(self, x, x_cls):
+        
+        u = torch.cat((x_cls,x),dim=1)
+        
+        
+        x_cls = x_cls + self.drop_path(self.gamma_1 * self.attn(self.norm1(u)))
+        
+        x_cls = x_cls + self.drop_path(self.gamma_2 * self.mlp(self.norm2(x_cls)))
+        
+        return x_cls 
+
+
 class ConvResidual(nn.Module):
     def __init__(self, dim, kernel_depthwise = 1, kernel_pointwise = 1):
         super().__init__()
@@ -68,33 +135,6 @@ class ConvResidual(nn.Module):
         x = x.transpose(-1,-2)
         return x
 
-
-class GlobalFilter(nn.Module):
-    def __init__(self, dim, h=14, w=8):
-        super().__init__()
-        self.complex_weight = nn.Parameter(torch.randn(h, w, dim, 2, dtype=torch.float32) * 0.02)
-        self.w = w
-        self.h = h
-
-    def forward(self, x, spatial_size=None):
-        B, N, C = x.shape
-        if spatial_size is None:
-            a = b = int(math.sqrt(N))
-        else:
-            a, b = spatial_size
-
-        x = x.view(B, a, b, C)
-
-        x = x.to(torch.float32)
-
-        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
-        weight = torch.view_as_complex(self.complex_weight)
-        x = x * weight
-        x = torch.fft.irfft2(x, s=(a, b), dim=(1, 2), norm='ortho')
-
-        x = x.reshape(B, N, C)
-
-        return x
 
 class Block(nn.Module):
 
@@ -149,32 +189,15 @@ class PatchEmbed(nn.Module):
             f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
         x = self.proj(x).flatten(2).transpose(1, 2)
         return x
-
-
-class DownLayer(nn.Module):
-    """ Image to Patch Embedding
-    """
-    def __init__(self, img_size=56, dim_in=64, dim_out=128):
-        super().__init__()
-        self.img_size = img_size
-        self.dim_in = dim_in
-        self.dim_out = dim_out
-        self.proj = nn.Conv2d(dim_in, dim_out, kernel_size=2, stride=2)
-        self.num_patches = img_size * img_size // 4
-
-    def forward(self, x):
-        B, N, C = x.size()
-        x = x.view(B, self.img_size, self.img_size, C).permute(0, 3, 1, 2)
-        x = self.proj(x).permute(0, 2, 3, 1)
-        x = x.reshape(B, -1, self.dim_out)
-        return x
-
+        
 class WayNet(nn.Module):
     
     def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dim=768, depth=12,
-                 representation_size=None, uniform_drop=False,
-                 drop_rate=0., drop_path_rate=0., norm_layer=None, 
-                 dropcls=0):
+                 representation_size=None, uniform_drop=False, multiclass=False, 
+                 block_layers_token = LayerScale_Block_CA,drop_rate=0., 
+                 drop_path_rate=0., norm_layer=None, num_heads=12,act_layer=nn.GELU,
+                 init_scale=1e-4, mlp_ratio_clstk = 4.0, qkv_bias=False, qk_scale=None,
+                 depth_token_only = 2, dropcls=0):
         """
         Args:
             img_size (int, tuple): input image size
@@ -203,6 +226,11 @@ class WayNet(nn.Module):
                 img_size=img_size, patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
         num_patches = self.patch_embed.num_patches
 
+        if not self.multiclass:
+            self.cls_token = nn.Parameter(torch.zeros(1, 1, int(embed_dim)))
+        else:
+            self.cls_token = nn.Parameter(torch.zeros(1, num_classes, int(embed_dim)))
+
         self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
         self.pos_drop = nn.Dropout(p=drop_rate)
 
@@ -223,6 +251,14 @@ class WayNet(nn.Module):
                 drop_path=dpr[i], norm_layer=norm_layer, 
                  h=h, w=w)
             for i in range(depth)])
+
+        self.blocks_token_only = nn.ModuleList([
+            block_layers_token(
+                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio_clstk, qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=0.0, attn_drop=0.0, drop_path=0.0, norm_layer=norm_layer,
+                act_layer=act_layer,Attention_block=Attention_block_token_only,
+                Mlp_block=Mlp_block_token_only,init_values=init_scale)
+            for i in range(depth_token_only)])
         
         self.norm = norm_layer(embed_dim)
 
@@ -236,6 +272,8 @@ class WayNet(nn.Module):
         else:
             self.pre_logits = nn.Identity()
 
+        self.feature_info = [dict(num_chs=embed_dim, reduction=0, module='head')]
+
         # Classifier head
         self.head = nn.Linear(self.num_features, num_classes) if num_classes > 0 else nn.Identity()
 
@@ -245,6 +283,8 @@ class WayNet(nn.Module):
         else:
             self.final_dropout = nn.Identity()
 
+        #normalize cls token and position emb - it's important!!!!
+        trunc_normal_(self.cls_token, std=.02)
         trunc_normal_(self.pos_embed, std=.02)
         self.apply(self._init_weights)
 
@@ -269,7 +309,9 @@ class WayNet(nn.Module):
         self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
 
     def forward_features(self, x):
+
         B = x.shape[0]
+        cls_tokens = self.cls_token.expand(B, -1, -1)  
         x = self.patch_embed(x)
         x = x + self.pos_embed
         x = self.pos_drop(x)
@@ -277,136 +319,12 @@ class WayNet(nn.Module):
         for blk in self.blocks:
             x = blk(x)
 
-        x = self.norm(x).mean(1)
-        return x
+        for i , blk in enumerate(self.blocks_token_only):
+            cls_tokens = blk(x,cls_tokens)
 
-    def forward(self, x):
-        x = self.forward_features(x)
-        x = self.final_dropout(x)
-        x = self.head(x)
-        return x
-
-
-class GFNetPyramid(nn.Module):
-    
-    def __init__(self, img_size=224, patch_size=4, num_classes=1000, embed_dim=[64, 128, 256, 512], depth=[2,2,10,4],
-                 mlp_ratio=[4, 4, 4, 4],
-                 drop_rate=0., drop_path_rate=0., norm_layer=None, init_values=0.001, no_layerscale=False, dropcls=0):
-        """
-        Args:
-            img_size (int, tuple): input image size
-            patch_size (int, tuple): patch size
-            in_chans (int): number of input channels
-            num_classes (int): number of classes for classification head
-            embed_dim (int): embedding dimension
-            depth (int): depth of transformer
-            num_heads (int): number of attention heads
-            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
-            qkv_bias (bool): enable bias for qkv if True
-            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
-            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
-            drop_rate (float): dropout rate
-            attn_drop_rate (float): attention dropout rate
-            drop_path_rate (float): stochastic depth rate
-            hybrid_backbone (nn.Module): CNN backbone to use in-place of PatchEmbed module
-            norm_layer: (nn.Module): normalization layer
-        """
-        super().__init__()
-        self.num_classes = num_classes
-        self.num_features = self.embed_dim = embed_dim[-1]  # num_features for consistency with other models
-        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
-
-        self.patch_embed = nn.ModuleList()
-        
-        patch_embed = PatchEmbed(
-                img_size=img_size, patch_size=patch_size, in_chans=3, embed_dim=embed_dim[0])
-        num_patches = patch_embed.num_patches
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim[0]))
-
-        self.patch_embed.append(patch_embed)
-
-        sizes = [56, 28, 14, 7]
-        for i in range(4):
-            sizes[i] = sizes[i] * img_size // 224
-
-        for i in range(3):
-            patch_embed = DownLayer(sizes[i], embed_dim[i], embed_dim[i+1])
-            num_patches = patch_embed.num_patches
-            self.patch_embed.append(patch_embed)
-
-
-        self.pos_drop = nn.Dropout(p=drop_rate)
-        self.blocks = nn.ModuleList()
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depth))]  # stochastic depth decay rule
-        cur = 0
-        for i in range(4):
-            h = sizes[i]
-            w = h // 2 + 1
-
-            if no_layerscale:
-                print('using standard block')
-                blk = nn.Sequential(*[
-                    Block(
-                    dim=embed_dim[i], mlp_ratio=mlp_ratio[i],
-                    drop=drop_rate, drop_path=dpr[cur + j], norm_layer=norm_layer, h=h, w=w)
-                for j in range(depth[i])
-                ])
-            else:
-                print('using layerscale block')
-                blk = nn.Sequential(*[
-                    BlockLayerScale(
-                    dim=embed_dim[i], mlp_ratio=mlp_ratio[i],
-                    drop=drop_rate, drop_path=dpr[cur + j], norm_layer=norm_layer, h=h, w=w, init_values=init_values)
-                for j in range(depth[i])
-                ])
-            self.blocks.append(blk)
-            cur += depth[i]
-
-        # Classifier head
-        self.norm = norm_layer(embed_dim[-1])
-
-        self.head = nn.Linear(self.num_features, num_classes)
-
-        if dropcls > 0:
-            print('dropout %.2f before classifier' % dropcls)
-            self.final_dropout = nn.Dropout(p=dropcls)
-        else:
-            self.final_dropout = nn.Identity()
-
-        trunc_normal_(self.pos_embed, std=.02)
-        self.apply(self._init_weights)
-
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'pos_embed', 'cls_token'}
-
-    def get_classifier(self):
-        return self.head
-
-    def reset_classifier(self, num_classes, global_pool=''):
-        self.num_classes = num_classes
-        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-    def forward_features(self, x):
-        for i in range(4):
-            x = self.patch_embed[i](x)
-            if i == 0:
-                x = x + self.pos_embed
-            x = self.blocks[i](x)
-
-        x = self.norm(x).mean(1)
-        return x
+        x = torch.cat((cls_tokens, x), dim=1)       
+        x = self.norm(x)
+        return x[:, 0]
 
     def forward(self, x):
         x = self.forward_features(x)
