ee37b32d7fee6072f8fd7ccf7f2b047bf5149e1b
Wayner Barrios <waybarrios@gmail.com>
2022-02-17 21:27:35 -0500

    adding cifar training

---
+++ cifar10_train.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+import time
+import argparse
+from waynet import WayNet
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--name', type=str, default="WayNet")
+parser.add_argument('--input-size', default=32, type=int, help='images input size')
+
+parser.add_argument('--batch-size', default=512, type=int)
+parser.add_argument('--scale', default=0.75, type=float)
+parser.add_argument('--reprob', default=0.25, type=float)
+parser.add_argument('--ra-m', default=8, type=int)
+parser.add_argument('--ra-n', default=1, type=int)
+parser.add_argument('--jitter', default=0.1, type=float)
+
+parser.add_argument('--depth', default=12, type=int)
+parser.add_argument('--patch-size', default=2, type=int)
+
+parser.add_argument('--wd', default=0.01, type=float)
+parser.add_argument('--clip-norm', action='store_true')
+parser.add_argument('--epochs', default=25, type=int)
+parser.add_argument('--lr-max', default=0.01, type=float)
+parser.add_argument('--workers', default=2, type=int)
+
+args = parser.parse_args()
+
+cifar10_mean = (0.4914, 0.4822, 0.4465)
+cifar10_std = (0.2471, 0.2435, 0.2616)
+
+train_transform = transforms.Compose([
+    transforms.RandomResizedCrop(32, scale=(args.scale, 1.0), ratio=(1.0, 1.0)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandAugment(num_ops=args.ra_n, magnitude=args.ra_m),
+    transforms.ColorJitter(args.jitter, args.jitter, args.jitter),
+    transforms.ToTensor(),
+    transforms.Normalize(cifar10_mean, cifar10_std),
+    transforms.RandomErasing(p=args.reprob)
+])
+
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(cifar10_mean, cifar10_std)
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=train_transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size,
+                                          shuffle=True, num_workers=args.workers)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=test_transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size,
+                                         shuffle=False, num_workers=args.workers)
+
+
+
+model = WayNet(
+            img_size=args.input_size, 
+            patch_size=args.patch_size, embed_dim=384, depth=args.depth,
+            norm_layer=partial(nn.LayerNorm, eps=1e-6)
+        )
+
+model = nn.DataParallel(model).cuda()
+
+
+lr_schedule = lambda t: np.interp([t], [0, args.epochs*2//5, args.epochs*4//5, args.epochs], 
+                                  [0, args.lr_max, args.lr_max/20.0, 0])[0]
+
+opt = optim.AdamW(model.parameters(), lr=args.lr_max, weight_decay=args.wd)
+criterion = nn.CrossEntropyLoss()
+scaler = torch.cuda.amp.GradScaler()
+
+
+for epoch in range(args.epochs):
+    start = time.time()
+    train_loss, train_acc, n = 0, 0, 0
+    for i, (X, y) in enumerate(trainloader):
+        model.train()
+        X, y = X.cuda(), y.cuda()
+
+        lr = lr_schedule(epoch + (i + 1)/len(trainloader))
+        opt.param_groups[0].update(lr=lr)
+
+        opt.zero_grad()
+        with torch.cuda.amp.autocast():
+            output = model(X)
+            loss = criterion(output, y)
+
+        scaler.scale(loss).backward()
+        if args.clip_norm:
+            scaler.unscale_(opt)
+            nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        scaler.step(opt)
+        scaler.update()
+        
+        train_loss += loss.item() * y.size(0)
+        train_acc += (output.max(1)[1] == y).sum().item()
+        n += y.size(0)
+        
+    model.eval()
+    test_acc, m = 0, 0
+    with torch.no_grad():
+        for i, (X, y) in enumerate(testloader):
+            X, y = X.cuda(), y.cuda()
+            with torch.cuda.amp.autocast():
+                output = model(X)
+            test_acc += (output.max(1)[1] == y).sum().item()
+            m += y.size(0)
+
+    print(f'[{args.name}] Epoch: {epoch} | Train Acc: {train_acc/n:.4f}, Test Acc: {test_acc/m:.4f}, Time: {time.time() - start:.1f}, lr: {lr:.6f}')
